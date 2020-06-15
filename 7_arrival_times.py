0d5476dedb8df1f8e1b6d34df7c876dc3267b840
aausuman <aausuman.26@gmail.com>
2020-06-15 16:40:02 +0530

    Arrival Times file : making some changes

--- 7_arrival_times.py
+++ 7_arrival_times.py
@@ -53,8 +53,8 @@ records_keyLineID_rdd = records_df.rdd.map(lambda x: (int(str(x["LineID"])), [(i
                                                                                int(float(str(x["Timestamp"]))), \
                                                                                str(x["JourneyPatternID"]), \
                                                                                int(str(x["VehicleJourneyID"])), \
-                                                                               int(str(x["VehicleID"])), \
                                                                                int(str(x["StopID"])), \
+                                                                               int(str(x["VehicleID"])), \
                                                                                str(x["Lat"]), \
                                                                                str(x["Lon"]), \
                                                                                int(str(x["AtStop"])), \
@@ -64,7 +64,7 @@ records_keyLineID_rdd = records_df.rdd.map(lambda x: (int(str(x["LineID"])), [(i
 reduced_byLineID_rdd = records_keyLineID_rdd.reduceByKey(lambda a, b: a + b)
 reduced_byLineID_list = reduced_byLineID_rdd.collect()
 
-# Iterating over each LineID
+# Iterating by LineID
 for element in reduced_byLineID_list:
     if element[0] == 747:
         within_lineID_rdd = sc.parallelize(element[1])
@@ -73,7 +73,7 @@ for element in reduced_byLineID_list:
         reduced_byJPID_rdd = within_lineID_keyJPID_rdd.reduceByKey(lambda a, b: a + b)
         reduced_byJPID_list = reduced_byJPID_rdd.collect()
 
-        # Iterating over each LineID's separate JourneyPatternID
+        # Iterating by LineID's separate JourneyPatternID
         for element_2 in reduced_byJPID_list:
             if element_2[0] == '7470001':
                 within_JPID_rdd = sc.parallelize(element_2[1])
@@ -82,20 +82,35 @@ for element in reduced_byLineID_list:
                 reduced_byVJID_rdd = within_JPID_keyVJID_rdd.reduceByKey(lambda a, b: a + b)
                 reduced_byVJID_list = reduced_byVJID_rdd.collect()
 
-                # Iterating over each LineID's separate JourneyPatternID's separate VehicleJourneyID
+                # Iterating by LineID's separate JourneyPatternID's separate VehicleJourneyID
                 for element_3 in reduced_byVJID_list:
-                    # if element_3[0] == 3665:
-                    within_StopID_rdd = sc.parallelize(element_3[1])
-                    within_StopID_keyTimestamp_rdd = within_StopID_rdd.map(lambda x: (x[1], (x[0], x[1], x[2], \
-                                                                                              x[3], x[4], x[5], \
-                                                                                              x[6], x[7], x[8], x[9])))
-                    sorted_byTimestamp_rdd = within_StopID_keyTimestamp_rdd.sortByKey().values()
-                    sorted_byTimestamp_df = sorted_byTimestamp_rdd.toDF(schema = ["LineID", "Timestamp", \
-                                                                                  "JourneyPatternID", \
-                                                                                  "VehicleJourneyID", \
-                                                                                  "VehicleID", "StopID", "Lat", "Lon", \
-                                                                                  "AtStop", "Delay"])
-                    sorted_byTimestamp_df.registerTempTable("records")
-                    filtered_df = sqc.sql("select * from records where AtStop = 1")
-                    filtered_df.show(100, False)
+                    if element_3[0] == 3493:
+                        within_VJID_rdd = sc.parallelize(element_3[1])
+                        within_VJID_keyStopID_rdd = within_VJID_rdd.map(lambda x: (x[4], [(x[0], x[1], x[2], x[3], \
+                                                                                           x[4], x[5], x[6], x[7], \
+                                                                                           x[8], x[9])]))
+                        reduced_byStopID_rdd = within_VJID_keyStopID_rdd.reduceByKey(lambda a, b: a + b)
+                        reduced_byStopID_list = reduced_byStopID_rdd.collect()
 
+                        # Iterating by LineID's separate JourneyPatternID's separate VehicleJourneyID's separate StopID
+                        for element_4 in reduced_byStopID_list:
+                            within_StopID_rdd = sc.parallelize(element_4[1])
+                            within_StopID_keyTimestamp_rdd = within_StopID_rdd.map(lambda x: (x[1], (x[0], x[1], \
+                                                                                                     x[2], x[3], \
+                                                                                                     x[4], x[5], \
+                                                                                                     x[6], x[7], \
+                                                                                                     x[8], x[9])))
+                            sorted_byTimestamp_rdd = within_StopID_keyTimestamp_rdd.sortByKey().values()
+                            sorted_byTimestamp_df = sorted_byTimestamp_rdd.toDF(schema = ["LineID", "Timestamp", \
+                                                                                          "JourneyPatternID", \
+                                                                                          "VehicleJourneyID", \
+                                                                                          "StopID", "VehicleID", \
+                                                                                          "Lat", "Lon", \
+                                                                                          "AtStop", "Delay"])
+                            sorted_byTimestamp_df.registerTempTable("records")
+                            filtered_df = sqc.sql("select * from records where AtStop = 1")
+                            filtered_df.show()
+                            # This will give us properly structured dfs in each loop
+                            # Each one will contain records of a Stop ID, within a VJID, within a JPID, within a LineID
+                            # only records when the AtStop value is equal to 1
+                            # We further need to think about the Timestamp of different days to compare Delays now
