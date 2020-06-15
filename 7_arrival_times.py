6a7a05666ffb8edb67861986a3b05885becef540
aausuman <aausuman.26@gmail.com>
2020-06-15 13:52:04 +0530

    Arrival Times file : added some more code

--- 7_arrival_times.py
+++ 7_arrival_times.py
@@ -16,6 +16,9 @@ sqc = SQLContext(sc)
 
 # Importing Dataset (Local machine)
 raw_records = sc.textFile("/Users/aausuman/Downloads/Thesis Dataset/siri.20130101.csv")
+
+# Importing Dataset (Databricks cluster)
+# raw_records = sc.textFile("/FileStore/tables/siri_20130101-aa346.csv")
 # Template for reading and concatenating the entire month's data
 # folder_path = "/Users/aausuman/Downloads/Thesis Dataset/"
 # files = os.listdir(folder_path)
@@ -24,10 +27,6 @@ raw_records = sc.textFile("/Users/aausuman/Downloads/Thesis Dataset/siri.2013010
 #     if file != '.DS_Store':
 #         records = sc.textFile(folder_path + file)
 #         raw_records = raw_records.union(records)
-
-# Importing Dataset (Databricks cluster)
-# raw_records = sc.textFile("/FileStore/tables/siri_20130101-aa346.csv")
-# need to process all files on cluster as local machine won't be handle that much
 
 # Function to extract fields from our comma separated data files
 def pre_process(record):
@@ -66,32 +65,38 @@ reduced_byLineID_list = reduced_byLineID_rdd.collect()
 
 # Iterating over each LineID
 for element in reduced_byLineID_list:
-    # if element[0] == 747:
-    within_lineID_rdd = sc.parallelize(element[1])
-    within_lineID_keyVJID_rdd = within_lineID_rdd.map(lambda x: (x[1], [(x[0], x[1], x[2], x[3], x[4], \
-                                                                         x[5], x[6], x[7], x[8])]))
-    reduced_byVJID_rdd = within_lineID_keyVJID_rdd.reduceByKey(lambda a, b: a + b)
-    reduced_byVJID_list = reduced_byVJID_rdd.collect()
+    if element[0] == 747:
+        within_lineID_rdd = sc.parallelize(element[1])
+        within_lineID_keyVJID_rdd = within_lineID_rdd.map(lambda x: (x[1], [(x[0], x[1], x[2], x[3], x[4], \
+                                                                             x[5], x[6], x[7], x[8])]))
+        reduced_byVJID_rdd = within_lineID_keyVJID_rdd.reduceByKey(lambda a, b: a + b)
+        reduced_byVJID_list = reduced_byVJID_rdd.collect()
 
-    # Iterating over each LineID's separate VehicleJourneyID
-    for element_2 in reduced_byVJID_list:
-        # if element_2[0] == 3493:
-        within_VJID_rdd = sc.parallelize(element_2[1])
-        within_VJID_keyStopID_rdd = within_VJID_rdd.map(lambda x: (x[2], [(x[0], x[1], x[2], x[3], x[4], \
-                                                                    x[5], x[6], x[7], x[8])]))
-        reduced_byStopID_rdd = within_VJID_keyStopID_rdd.reduceByKey(lambda a, b: a + b)
-        reduced_byStopID_list = reduced_byStopID_rdd.collect()
+        # Iterating over each LineID's separate VehicleJourneyID
+        for element_2 in reduced_byVJID_list:
+            if element_2[0] == 3493:
+                within_VJID_rdd = sc.parallelize(element_2[1])
+                within_VJID_keyStopID_rdd = within_VJID_rdd.map(lambda x: (x[2], [(x[0], x[1], x[2], x[3], x[4], \
+                                                                            x[5], x[6], x[7], x[8])]))
+                reduced_byStopID_rdd = within_VJID_keyStopID_rdd.reduceByKey(lambda a, b: a + b)
+                reduced_byStopID_list = reduced_byStopID_rdd.collect()
 
-        # Iterating over each LineID's separate VehicleJourneyID's separate StopID
-        for element_3 in reduced_byStopID_list:
-            # if element_3[0] == 7411:
-            within_StopID_rdd = sc.parallelize(element_3[1])
-            within_StopID_keyTimestamp_rdd = within_StopID_rdd.map(lambda x: (x[3], (x[0], x[1], x[2], \
-                                                                                      x[3], x[4], x[5], \
-                                                                                      x[6], x[7], x[8])))
-            sorted_byTimestamp_rdd = within_StopID_keyTimestamp_rdd.sortByKey().values()
-            sorted_byTimestamp_df = sorted_byTimestamp_rdd.toDF(schema = ["LineID", "VehicleJourneyID", \
-                                                                               "StopID", "Timestamp", \
-                                                                               "VehicleID", "Lat", "Lon", \
-                                                                               "AtStop", "Delay"])
-            # sorted_byTimestamp_df.show()
+                # Iterating over each LineID's separate VehicleJourneyID's separate StopID
+                for element_3 in reduced_byStopID_list:
+                    # if element_3[0] == 3665:
+                    within_StopID_rdd = sc.parallelize(element_3[1])
+                    within_StopID_keyTimestamp_rdd = within_StopID_rdd.map(lambda x: (x[3], (x[0], x[1], x[2], \
+                                                                                              x[3], x[4], x[5], \
+                                                                                              x[6], x[7], x[8])))
+                    sorted_byTimestamp_rdd = within_StopID_keyTimestamp_rdd.sortByKey().values()
+                    sorted_byTimestamp_df = sorted_byTimestamp_rdd.toDF(schema = ["LineID", "VehicleJourneyID", \
+                                                                                       "StopID", "Timestamp", \
+                                                                                       "VehicleID", "Lat", "Lon", \
+                                                                                       "AtStop", "Delay"])
+                    # sorted_byTimestamp_df.show()
+                    sorted_byTimestamp_df.registerTempTable("records")
+                    filtered_df = sqc.sql("select LineID, VehicleJourneyID, StopID, Timestamp, AtStop, Delay \
+                     from records where AtStop = 1")
+                    # The above filtered df now consists of the records when a bus running on a particular line
+                    # within a vehicle journey id has reached and stayed at a stop
+
